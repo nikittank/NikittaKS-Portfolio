2d74a98561b914171e93475cd25b51ee8daca007
nikittank <saadmyhabibi@gmail.com>
2025-06-22 14:58:00 +0530

    Initial commit

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -27,7 +27,7 @@ const Container = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
-  background: rgba(10, 15, 20, 0.85);
+  background: rgba(0, 0, 0, 0.85);
   z-index: 9999;
   backdrop-filter: blur(3px);
 `;

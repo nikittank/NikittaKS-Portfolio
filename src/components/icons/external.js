388ab9c04d2ab53f7556dcc5d4cfa1f3d325372a
Nikitta K S <91609970+nikittank@users.noreply.github.com>
2025-10-25 13:46:51 +0530

    Refactor IconExternal to add a wrapper div
    Update IconExternal component to include a wrapper div.

--- src/components/icons/external.js
+++ src/components/icons/external.js
@@ -2,7 +2,7 @@ import React from "react";
 import { LuLink } from "react-icons/lu";
 
 const IconExternal = () => (
-  <div style={{ backgroundColor: "black", display: "inline-block", padding: "5px", borderRadius: "4px" }}>
+  <div>
     <LuLink size={22} color="white" />
   </div>
 );

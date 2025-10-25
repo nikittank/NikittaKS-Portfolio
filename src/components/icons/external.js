95b190762ffd37ed8a981a2e3b75b35b7a113b47
Nikitta K S <91609970+nikittank@users.noreply.github.com>
2025-10-25 14:20:57 +0530

    Refactor SVG path for IconExternal component

--- src/components/icons/external.js
+++ src/components/icons/external.js
@@ -2,16 +2,16 @@ import React from "react";
 
 const IconExternal = () => (
   <svg
+    width="48"
+    height="48"
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
   >
     <path
-      d="M14 7H16C18.7614 7 21 9.23858 21 12C21 14.7614 18.7614 17 16 17H14M10 7H8C5.23858 7 3 9.23858 3 12C3 14.7614 5.23858 17 8 17H10M8 12H16"
-      stroke="white"        // white stroke for black background
-      strokeWidth="2"       // React camelCase
+      d="M8 12H16M10 7H8C5.23858 7 3 9.23858 3 12C3 14.7614 5.23858 17 8 17H10M14 7H16C18.7614 7 21 9.23858 21 12C21 14.7614 18.7614 17 16 17H14"
+      stroke="white"
+      strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
     />

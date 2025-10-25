37ca51cd75bd1f88b02c1d56200ec993547f08ac
Nikitta K S <91609970+nikittank@users.noreply.github.com>
2025-10-25 14:16:55 +0530

    Update external.js

--- src/components/icons/external.js
+++ src/components/icons/external.js
@@ -10,8 +10,8 @@ const IconExternal = () => (
   >
     <path
       d="M14 7H16C18.7614 7 21 9.23858 21 12C21 14.7614 18.7614 17 16 17H14M10 7H8C5.23858 7 3 9.23858 3 12C3 14.7614 5.23858 17 8 17H10M8 12H16"
-      stroke="white"      // <-- white stroke for black background
-      strokeWidth="2"     // React camelCase
+      stroke="white"        // white stroke for black background
+      strokeWidth="2"       // React camelCase
       strokeLinecap="round"
       strokeLinejoin="round"
     />

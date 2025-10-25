aa3ef11e5e7f19a66b5196bfb6af5c06632d483b
Nikitta K S <91609970+nikittank@users.noreply.github.com>
2025-10-25 13:42:41 +0530

    Style IconExternal with background and color
    Updated IconExternal to include styling and change icon color.

--- src/components/icons/external.js
+++ src/components/icons/external.js
@@ -2,8 +2,8 @@ import React from "react";
 import { LuLink } from "react-icons/lu";
 
 const IconExternal = () => (
-  <div>
-    <LuLink size={22} />
+  <div style={{ backgroundColor: "black", display: "inline-block", padding: "5px", borderRadius: "4px" }}>
+    <LuLink size={22} color="white" />
   </div>
 );
 
